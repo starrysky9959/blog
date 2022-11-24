759a30392d7da906a288428d4f5f8c25feed4e59
starrysky9959 <starrysky9651@outlook.com>
2022-11-25 00:49:53 +0800

    change route mode to history

--- push.sh
+++ push.sh
@@ -1,9 +1,9 @@
-git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file"; done
 ###
  # @Author: starrysky9959 starrysky9651@outlook.com
  # @Date: 2022-11-25 00:14:38
  # @LastEditors: starrysky9959 starrysky9651@outlook.com
- # @LastEditTime: 2022-11-25 00:14:37
+ # @LastEditTime: 2022-11-25 00:21:32
  # @Description:  
 ### 
+git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file"; done
 git push origin master
